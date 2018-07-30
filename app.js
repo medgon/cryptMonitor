fc106d829f31ec108e36b662428bce8bbf8e2a22
medgon <minhtly1@gmail.com>
2018-07-29 23:42:41 -0400

    Got list to display

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.get("/", function(req, res){
     res.render("search");
 });
 
-app.get("/results", function(req, res){
+app.get("/list", function(req, res){
     var query = req.query.search;
     var url = "https://bittrex.com/api/v1.1/public/getmarkets";
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body)
-            res.send(data["result"])
-      //      res.render("results", {data: data});
+            res.render("list", {data : data});
+          //  res.send(data["result"]);
         }
     });
 });
@@ -25,6 +25,6 @@ app.listen(process.env.PORT, process.env.IP, function () {
 });*/
 
 /*Specify what port you want this app to run on locally*/
-app.listen(5000, function () {
+app.listen(8080, function () {
     console.log("App has started");
 });
